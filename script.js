b92f273158566bb1e686ac80c8d51fa517761638
Jessica Lee <JessLee@Jess-MacBook-Pro.local>
2016-06-03 16:47:19 -0600

    board gradient, finito

--- script.js
+++ script.js
@@ -6,10 +6,13 @@ var div_wrapper = document.createElement("div")
 var div_pink  = document.createElement("div");
 var div_mint = document.createElement("div");
 
-div_wrapper.style.backgroundColor=-webkit-linear-gradient(top, white, #000);
-gotBody.appendChild(div_wrapper)
-
-
+// div_wrapper.style.backgroundColor= "-webkit-linear-gradient linear-gradien(to bottom right,#002f4b,#dc4225)";
+div_wrapper.style.background= "linear-gradient(white, grey)";
+div_wrapper.style.width = "100vw";
+div_wrapper.style.height = "108vh";
+// div_wrapper.style.zIndex = "10";
+div_wrapper.style.position = "absolute";
+gotBody.appendChild(div_wrapper);
 
 
 for (var i = 0; i < 63; i++) {
@@ -21,8 +24,9 @@ for (var i = 0; i < 63; i++) {
   div_pink.style.float = "left";
   div_pink.style.paddingBottom = "11.1%";
   div_pink.style.margin = "0px";
+  div_pink.style.opacity = "0.5"
 
-  div_wrapper.appendChild(div_pink);
+  gotBody.appendChild(div_pink);
 
   } else {
     var div_mint = document.createElement("div");
@@ -33,7 +37,10 @@ for (var i = 0; i < 63; i++) {
     div_mint.style.paddingBottom = "11.1%";
     div_mint.style.margin = "0px";
 
-    div_wrapper.appendChild(div_mint);
+    div_mint.style.opacity = "0.5"
+
+
+    gotBody.appendChild(div_mint);
 
   }
 };
